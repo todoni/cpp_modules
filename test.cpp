a6f74893b793c987e8670e31d3c317d5b09375ca
sohan <sohan@student.42seoul.kr>
2022-07-19 20:26:39 +0900

    [fix] const

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Object
+{
+protected:
+	int xpos, ypos;
+public:
+	virtual void Move(int x, int y) = 0;
+	void Trade(void);
+};
+
+class Player : public Object
+{
+private:
+	int hp;
+public:
+	virtual void	Move(int x, int y)
+	{
+		std::cout << "Player Move" << std::endl;
+	}
+	void	Trade(void)
+};
+
+class Monster : public Object
+{
+	public:
+	//virtual void	Move(int x, int y)
+	//{
+//		std::cout << "Monster Move" << std::endl;
+//	}
+};
+
+int	main(void)
+{
+
+	int		k = 42;
+	int	*ptr = &k;
+
+	Object	*obj[3];
+	obj[0] = new Monster();
+	obj[1] = new Monster();
+	obj[2] = new Player();
+	obj[2]->Trade();
+	for(int iCnt = 0; iCnt < 3; iCnt++)
+		Move(1, 2);
+	return (0);	
+}
